42d7cf7f6e5b1c8a20c856bd96b47ca86ad9fe29
nityam12 <nityam1050@gmail.com>
2020-07-12 01:08:47 -0700

    Updates

---
+++ precache-manifest.ee3db2462815860cd5bc558e693b97f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18563f54599d343fff6f89b5e99c2c8",
+    "url": "/sample1/index.html"
+  },
+  {
+    "revision": "137809c1855a98e75dfa",
+    "url": "/sample1/static/css/main.0fa2658e.chunk.css"
+  },
+  {
+    "revision": "640c40c8ff76cc21220d",
+    "url": "/sample1/static/js/2.11ca4bc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample1/static/js/2.11ca4bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137809c1855a98e75dfa",
+    "url": "/sample1/static/js/main.b01daed2.chunk.js"
+  },
+  {
+    "revision": "c7cb31733ffab22145f7",
+    "url": "/sample1/static/js/runtime-main.ff30af26.js"
+  }
+]);
